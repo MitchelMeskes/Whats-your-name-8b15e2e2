9b8196c4d76c0b90cc3eba6b58d1e63edcd6869a
Mitchel Meskes <2115621@talnet.nl>
2019-10-03 11:35:57 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hallo Gebruiker, Wat is je voornaam\n";
+$voornaam = readline();
+echo "En Wat Is Uw Achternaam?\n";
+$achternaam = readline();
+echo "Uw Naam is: " .$voornaam. " " .$achternaam;
+
+
+?>
